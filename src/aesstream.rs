85b4eb7273a43d832040ca930e7eaeb9da0f2f5b
Damir Jelić <poljar@termina.org.uk>
2019-11-04 10:03:11 +0100

    seshat: Add missing copyright headers.

--- src/aesstream.rs
+++ src/aesstream.rs
@@ -1,4 +1,5 @@
 // Copyright (c) 2015 The adb-remote-control Developers
+// Copyright (c) 2019 The Matrix.org Foundation CIC
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
