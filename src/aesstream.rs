235911945ba9c5268660fbee686fb5ec828e6650
Damir Jelić <poljar@termina.org.uk>
2019-11-06 14:07:18 +0100

    seshat: Fix the remaining copyright headers.

--- src/aesstream.rs
+++ src/aesstream.rs
@@ -1,5 +1,5 @@
 // Copyright (c) 2015 The adb-remote-control Developers
-// Copyright (c) 2019 The Matrix.org Foundation CIC
+// Copyright (c) 2019 The Matrix.org Foundation C.I.C.
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
